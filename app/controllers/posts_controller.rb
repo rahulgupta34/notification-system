ef140f4eb928bc4d1754e6e09cbf879769a37122
rahulgupta34 <rahul.g@simformsolutions.com>
2022-11-01 13:41:34 +0530

    Rails notification system [01/11/2022]
    modify show template for dashboard and index for notifications
     add mark_read action at notifications controller
    add routes for mark_read
    add broadcasting notification model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.all
-    @notifications = Notification.count
+    @notifications = Notification.unread.count
   end
 
   # GET /posts/1 or /posts/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts
-  resources :notifications, only: [ :index ]
+  resources :notifications, only: [ :index, :mark_read ] do
+    member do 
+      get :mark_read
+    end
+  end
   root "dashboard#show"
 end
